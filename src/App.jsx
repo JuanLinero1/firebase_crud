64122c16f04f1474dd59f917b990c0b3ff76938b
juanLinero <juanlinerobonnett611@gmail.com>
2023-04-08 09:18:46 -0500

    connected to firebase, created useffect for reading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react'
+import { db } from '../firebase_config'
+import { collection, getDocs } from 'firebase/firestore'
+
 import Create from './components/Create/Create'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const usersCollectionRef = collection(db, "Users")
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+      setUsers(data.docs.map(doc => ({...doc.data(), id: doc.id})))
+      console.log(users)
+    }
+    getUsers()
+  }, [])
+  
   return (
     <div className="App">
       <Create />
